f7aff82fc18033e62ce9ddda052173585d1a2462
Shibopie <dhalsuchismita7@gmail.com>
2020-04-18 09:48:18 +0530

    daily commit

--- 2020/02-February/02.01.py
+++ 2020/02-February/02.01.py
@@ -1,8 +1,8 @@
 # MICROSOFT
 """
+    READ DISSCUSSIONS -- LEETCODE#18
     Given a list of numbers, and a target number n, find all unique combinations
     of a, b, c, d, such that a + b + c + d = n.
-    // referred : https://www.techiedelight.com/4-sum-problem/
 """
 
 
@@ -10,42 +10,40 @@ def fourSum(nums, target):
     # Time: O(n^2)   Space: O(n^2)
     n = len(nums)
     t = target
-    nums.sort()
+    nums = sorted(nums)
+    print(nums)
 
-    sol = []
+    sol = list()
     pairsum = dict()
 
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            f = 0
+    # building dictionary
+    for i in range(n-1):
+        for j in range(i+1, n):
             sum = nums[i] + nums[j]
             if sum not in pairsum:
                 pairsum[sum] = [(nums[i], nums[j])]
             else:
-                for p in pairsum[sum]:
-                    if nums[i] == p[0]:
-                        f = 1
-                        break
-                if f == 0:
-                    pairsum[sum].append((nums[i], nums[j]))
+                pairsum[sum].append((nums[i], nums[j]))
 
-    # for sum in pairsum:
-    #     samesum = pairsum[sum]
-    #     for pair in samesum:
-    #         req = t - sum
-    #         if req in pairsum:
-    #             for p in pairsum[req]:
-    #                 if i > p[1]:
-    #                     sol.append(sorted([i, j, p[0], p[1]]))
+    # traversing all keys
+    for s in pairsum:
+        r = target - s
+        if r in pairsum:
+            for pairs in pairsum[s]:
+                max = pairs[1]-1
+                for pairr in pairsum[r]:
+                    if pairr[0] > max:
+                        sol.append([pairs[0], pairs[1], pairr[0], pairr[1]])
+                        max = pairr[0]
     print(pairsum)
     return sol
 
 
 print(fourSum([1, 1, -1, 0, -2, 1, -1], 0))
 # print [[-1, -1, 1, 1], [-2, 0, 1, 1]]
-print("//////////////////////////////////")
+
 print(fourSum([3, 0, 1, -5, 4, 0, -1], 1))
 # print [[-5, -1, 3, 4]]
-print("//////////////////////////////////")
+
 print(fourSum([0, 0, 0, 0, 0], 0))
 # print ([0, 0, 0, 0])
