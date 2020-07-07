6e353bccd2df895d04977afcd8b7b6e4d1ec6e32
Shibopie <dhalsuchismita7@gmail.com>
2020-07-07 05:31:06 +0530

    daily commit

--- 2020/04-April/04.01.py
+++ 2020/04-April/04.01.py
@@ -1,6 +1,6 @@
 # LINKEDIN
 """
-    SOLVED
+    SOLVED -- (simitar) LEETCODE#86
     Given a list of numbers and an integer k, 
     partition/sort the list such that the all numbers less than k occur before k, 
     and all numbers greater than k occur after the number k.
