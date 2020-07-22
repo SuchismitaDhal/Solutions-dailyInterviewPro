92a876f1fbe1808a0bffdf82c9412573827e7b41
Shibopie <dhalsuchismita7@gmail.com>
2020-07-22 11:36:14 +0530

    daily commit

--- 2020/02-February/02.27.py
+++ 2020/02-February/02.27.py
@@ -1,5 +1,6 @@
 # AMAZON
 """
+    SOLVED -- LEETCODE#621
     A task is a some work to be done which can be assumed takes 1 unit of time. 
     Between the same type of tasks you must take at least n units of time 
     before running the same tasks again.
@@ -10,12 +11,25 @@
 
 
 def schedule_tasks(tasks, n):
-    freq = [0] * 26
-    sol = 0
-    return sol
+    maxfreq = 0
+    e = 0
+
+    freq = dict()
+    for t in tasks:
+        if t in freq:
+            freq[t] += 1
+            maxfreq = max(maxfreq, freq[t])
+        else:
+            freq[t] = 1
+
+    for t in freq:
+        if freq[t] == maxfreq:
+            e += 1
+
+    return max(len(tasks), (maxfreq - 1) * (n + 1) + e)
 
 
-print(schedule_tasks(['q', 'q', 's', 'q', 'w', 'w'], 4))
+print(schedule_tasks(['q', 'q', 'w', 'w'], 3))
 # print 6
 # one of the possible orders to run the task would be
 # 'q', 'w', idle, idle, 'q', 'w'
---
+++ 2020/07-July/07.21.py
@@ -0,0 +1,47 @@
+# AIRBNB
+"""
+    SOLVED -- NO SIMILAR PROBLEM
+    Given a phone number, return all valid words 
+    that can be created using that phone number.
+    For instance, given the phone number 364
+    we can construct the words ['dog', 'fog'].
+"""
+
+lettersMaps = {
+    1: [],
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z'],
+    0: []
+}
+
+validWords = ['dog', 'fish', 'cat', 'fog']
+
+# if validWords changes frequently (is not same for many phone),
+# the brute force approach is better. For q queries [Time: O(q*n*m) Space: O(1)]
+# otherwise,
+# we can process all validwords once and create a segment tree
+# [Time: O(n*m) Space: O(n*m)] For q queries [Time: O(q*m) Space: O(1)]
+
+
+def makeWords_brute(phone):
+    sol = []
+    for w in validWords:
+        if len(w) == len(phone):
+            i = 0
+            while i < len(w):
+                if w[i] not in lettersMaps[int(phone[i])]:
+                    break
+                i += 1
+            if i == len(w):
+                sol.append(w)
+    return sol
+
+
+print(makeWords_brute('364'))
+# ['dog', 'fog']
