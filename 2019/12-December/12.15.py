c6b9f0c9311e5f29a0e0be47b98bf83b94532546
Shibopie <dhalsuchismita7@gmail.com>
2020-07-30 13:06:11 +0530

    daily commit

--- 2019/12-December/12.15.py
+++ 2019/12-December/12.15.py
@@ -1,6 +1,6 @@
 # GOOGLE
 """
-    SOLVED
+    SOLVED -- VERIFIED[https://www.programcreek.com/2013/02/leetcode-inorder-successor-in-bst-ii-java/]
     Given a node in a binary search tree (may not be the root), 
     find the next largest node in the binary search tree (also known as an inorder successor). 
     The nodes in this binary search tree will also have a parent field to traverse up the tree.
@@ -35,6 +35,7 @@ def inorder_successor(node):
         node = node.parent
 
     # node is the rightmost element
+    return None
 
 
 tree = Node(3)
