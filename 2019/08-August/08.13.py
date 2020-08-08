f4dd1b3b284df0966611ac2049169173713887f6
Shibopie <dhalsuchismita7@gmail.com>
2020-08-08 21:35:37 +0530

    daily commit

--- 2019/08-August/08.13.py
+++ 2019/08-August/08.13.py
@@ -1,5 +1,6 @@
 # GOOGLE
 """
+    SOLVED -- LEETCODE#224
     Given a mathematical expression with just single digits, 
     plus signs, negative signs, and brackets, evaluate the expression. 
     Assume the expression is properly formed.
@@ -9,62 +10,55 @@
 """
 
 
-def solve(exp):
-    if len(exp) <= 2:
-        return exp
-
-    # no negative integers
-    if len(exp) == 3:
-        if exp[1] == '+':
-            return int(exp[0]) + int(exp[2])
-        else:
-            return int(exp[0]) - int(exp[2])
-
-    # one negetive integer
-    if len(exp) == 4:
-        if exp[0] == '-':
-            if exp[2] == '+':
-                return - (int(exp[0]) - int(exp[2]))
-            else:
-                return - (int(exp[0]) + int(exp[2]))
-        else:
-            if exp[1] == '+':
-                return int(exp[0]) - int(exp[2])
-            else:
-                return int(exp[0]) + int(exp[2])
-
-    # two negetive integers
-    if len(exp) == 5:
-        if exp[2] == '+':
-            return -(int(exp[0]) + int(exp[2]))
-        else:
-            return -(int(exp[0]) - int(exp[2]))
-
-
-def eval(expression):
-    # Time: O(n)    Space: O(n)
-    stack = list()
-    e = list()
-
-    for i in range(len(expression)):
-        c = expression[i]
-        if c != ' ':
-            if c == ')':
-                p = stack.pop()
-                while p != '(':
-                    e.append(p)
-                    p = stack.pop()
-
-                stack.append(str(solve(e[::-1])))
-                e.clear()
-            else:
-                stack.append(c)
-
-    sol = ""
-    for c in stack:
-        sol += c
+def convertuni(e):
+    sol = []
+    i = 0
+    while i < len(e):
+        if e[i] >= '0' and e[i] <= '9':
+            num = 0
+            while i < len(e) and e[i] >= '0' and e[i] <= '9':
+                num = num * 10 + (ord(e[i]) - ord('0'))
+                i += 1
+            sol.append(num)
+        if i < len(e) and e[i] != ' ':
+            sol.append(e[i])
+        i += 1
+    print(sol)
     return sol
 
 
-print(eval('- (3 + ( 2 - 1 ) )'))
+def assignsign(e):
+    sol = []
+    neg = False
+    stack = list()
+
+    for i in range(len(e)):
+        if isinstance(e[i], int):
+            if i - 1 >= 0 and e[i - 1] == '-':
+                val = e[i] if neg else - e[i]
+            else:
+                val = -e[i] if neg else e[i]
+            sol.append(val)
+        if e[i] == '(':
+            if i - 1 >= 0 and e[i - 1] == '-':
+                stack.append('-')
+                neg = neg ^ True
+            else:
+                stack.append('+')
+        if e[i] == ')':
+            if stack[-1] == '-':
+                neg = neg ^ True
+            stack.pop()
+    return sol
+
+
+def eval(expression):
+    # Time: O(n)     Space: O(n)
+    exp = convertuni(expression)
+    exp = assignsign(exp)
+    return sum(exp)
+
+
+#print(eval('- (3 + ( 2 - 1 ) )'))
 # -4
+print(eval(" 2-1 + 2 "))
